798ddff516d9117caccade7626b97b80256ba2e9
pgasawa <pgasawa@gmail.com>
2024-02-16 22:03:31 -0800

    speech sends messages

--- socket_client.py
+++ socket_client.py
@@ -3,7 +3,7 @@ import socket
 HOST = '10.32.82.125'  # Replace with server's IP address
 PORT = 12345        # Same port as server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    message = "Hello from client!"
-    s.sendall(message.encode("utf-8"))
+def send_message(message: str):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(message.encode("utf-8"))
--- speech_to_text.py
+++ speech_to_text.py
@@ -1,15 +1,11 @@
 import speech_recognition as sr
-import pyaudio
-import time
+from socket_client import send_message
 
 init_rec = sr.Recognizer()
 print("Let's speak!!")
 with sr.Microphone() as source:
     audio_data = init_rec.record(source, duration=5)
     print("Recognizing your text...")
-    start = time.time()
     text = init_rec.recognize_google(audio_data)
-    end = time.time()
 
-    print(text)
-    print('Latency:', end - start, 'seconds')
+    send_message(text)
