1de463e5ff23b176e2b4a9b5edba22954a99f5a7
Ayushi Batwara <ayushibatwara@gmail.com>
2024-02-17 11:57:04 -0800

    finished 2 way socket

--- client/client.py
+++ client/client.py
@@ -1,30 +1,35 @@
 import socket
+import subprocess
 import speech_recognition as sr
 from timeit import default_timer as timer
 import asyncio
+import os
+from dotenv import load_dotenv
 
-# 1. Setup socket connection with server
+load_dotenv()
+HOST = os.environ["HOST"]
+PORT = int(os.environ["SERVER_LISTEN_PORT"])
 
-HOST = '10.32.80.163'  # Replace with server's IP address
-PORT = 12345        # Same port as server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.connect((HOST, PORT))
 
 # 2. Keep listening to new audio
-async def listen():
+def listen():
     recognizer = sr.Recognizer()
     with sr.Microphone() as source:
         # recognizer.adjust_for_ambient_noise(source)
         recognizer.pause_threshold = 0.5
+        try: 
+            while True:
+                print("Listening...")
+                audio = recognizer.listen(source)
+                transcribe(recognizer, audio)
+        except KeyboardInterrupt:
+            print("Notification receiving thread terminated.")
 
-        while True:
-            print("Listening...")
-            
-            audio = recognizer.listen(source)
-            await transcribe(recognizer, audio)
 
 # Transcribe the audio
-async def transcribe(recognizer, audio):
+def transcribe(recognizer, audio):
     try:
         start = timer()
         transcribed_words = recognizer.recognize_whisper(audio)
@@ -32,10 +37,11 @@ async def transcribe(recognizer, audio):
         print("Time elapsed:", end - start)
         print("You said:", transcribed_words)
         print("=============")
-        s.sendall(transcribed_words.encode("utf-8"))
+        socket.sendall(transcribed_words.encode("utf-8"))
+        print("HERE I AM TRYING TO RECEIVE DATA: ",socket.recv(1024).decode('utf-8'))
     except sr.UnknownValueError:
         print("Whisper Speech Recognition could not understand audio")
     except sr.RequestError as e:
         print("Could not request results from Whisper Speech Recognition service; {0}".format(e))
-
-asyncio.run(listen())
+            
+listen()
