fa2d2b0fcfa84693819251d103cf6a0901da57d0
pgasawa <pgasawa@gmail.com>
2024-02-17 14:11:44 -0800

    All client side calendar logic

--- client/client_agent_handler.py
+++ client/client_agent_handler.py
@@ -1,6 +1,7 @@
 import json
 import os
 from agents.mailing import draft_email
+from client.agents.scheduling import create_event
 from dotenv import load_dotenv
 import subprocess
 
@@ -17,3 +18,7 @@ def handle_response(response: str):
     if json_response['action'] == 'email':
         draft_email(json_response['recipient'], json_response['subject'], json_response['body'], USER_EMAIL)
         send_notification("Email drafted to " + json_response['recipient'] + " regarding " + json_response["subject"], "Email Drafted")
+
+    elif json_response['action'] == 'schedule':
+        draft_email(json_response['attendeeEmails'], json_response['startTime'], json_response['title'], json_response["description"])
+        send_notification(json_response["Title"] + " scheduled with " + json_response['attendeeEmails'] + " at " + json_response["startTime"], "Event Scheduled")
