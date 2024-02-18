90fceaf1bc8ea737fa47185acd9d47e12e8faa2f
pgasawa <pgasawa@gmail.com>
2024-02-18 01:03:47 -0800

    Making notification JSONs for frontend

--- client/client_agent_handler.py
+++ client/client_agent_handler.py
@@ -18,29 +18,42 @@ def send_notification(message, title="Notification Title"):
 
 def handle_response(response: str):
     json_response = json.loads(response)
+    notification = {}
     if json_response['action'] == 'email':
         draft_email(json_response['recipient'], json_response['subject'], json_response['body'], USER_EMAIL)
-        send_notification("Email drafted to " + json_response['recipient'] + " regarding " + json_response["subject"], "Email Drafted")
+        notification['title'] = "Email Drafted"
+        notification['body'] = "Email drafted to " + json_response['recipient'] + " regarding " + json_response["subject"]
+        # send_notification("Email drafted to " + json_response['recipient'] + " regarding " + json_response["subject"], "Email Drafted")
 
     elif json_response['action'] == 'link':
         if json_response['link'] != "":
-            send_notification(f"Does this link help: {json_response['link']}", json_response["description"])
+            notification['title'] = json_response['description']
+            notification['body'] = f"Does this link help: {json_response['link']}"
+            # send_notification(f"Does this link help: {json_response['link']}", json_response["description"])
         
     elif json_response['action'] == 'schedule':
         link = create_event(json_response['attendeeEmails'], json_response['startTime'], json_response['summary'], json_response["description"])
         start_time = datetime.strptime(json_response["startTime"], "%Y-%m-%dT%H:%M:%S")
         start_time = start_time.strftime("%b %d at %I:%M%p")
-        send_notification(f"{json_response['summary']} + scheduled at {start_time}. {link}", "Event Scheduled")
+        notification['title'] = "Event Scheduled"
+        notification['body'] = f"{json_response['summary']} + scheduled at {start_time}. {link}"
+        # send_notification(f"{json_response['summary']} + scheduled at {start_time}. {link}", "Event Scheduled")
 
     elif json_response['action'] == 'clarify':
         if json_response['result'] != "NONE":
-            send_notification(json_response['result'], "Quick Insight")
+            notification['title'] = "Quick Insight"
+            notification['body'] = json_response['result']
+            # send_notification(json_response['result'], "Quick Insight")
         else:
-            send_notification(search_google(json_response['search_query']), "More Info")
+            notification['title'] = "More Info"
+            notification['body'] = json_response['search_query']
+            # send_notification(search_google(json_response['search_query']), "More Info")
 
     elif json_response['action'] == 'assistant':
         speak(json_response['answer'])
             
     elif json_response['action'] == 'suggestion':
         if json_response['suggestion'] != []:
-            send_notification("\n\n".join(json_response['suggestion']), "Suggestions")
+            notification['title'] = "Suggestions"
+            notification['body'] = "\n\n".join(json_response['suggestion'])
+            # send_notification("\n\n".join(json_response['suggestion']), "Suggestions")
