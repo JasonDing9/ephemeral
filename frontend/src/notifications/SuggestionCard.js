425e78f0123bd91f8952f3eba61f040480f10073
pgasawa <pgasawa@gmail.com>
2024-02-18 05:47:03 -0800

    Finished suggestions frontend

--- frontend/src/notifications/SuggestionCard.js
+++ frontend/src/notifications/SuggestionCard.js
@@ -2,15 +2,20 @@ import React from 'react';
 import Notification from './Notification';
 
 function SuggestionCard(props) {
-    return (
+    const title = "💡 Assistant's Thoughts";
+    const subtitle = "";
+
+    console.log(props);
+
+    return props.suggestions.map((suggestion, index) => (
         <Notification
+            key={index}
             timestamp={props.timestamp}
-            title="💡 AI Assistant said"
-            subtitle="To break the ice, share one thing you are looking forward to this week."
-            suggestions=""
-            links={[]}
+            title={title}
+            subtitle={subtitle}
+            text={suggestion.replace(/^AI Assistant said: (.*)$/, '$1')}
         />
-    );
+    ));
 }
 
 export default SuggestionCard;
